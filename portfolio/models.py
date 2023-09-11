c35c6b1f85338f6f4aa0a6000ce779fbee7bce91
DIONI <dionisiomiron@gmail.com>
2023-09-11 15:05:10 +0100

    Versao 2.2

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+
+
+# Create your models here.
 class Post(models.Model):
     titulo = models.CharField(max_length=30)
     author = models.CharField(max_length=40)
@@ -7,5 +10,6 @@ class Post(models.Model):
     datacriacao = models.DateTimeField(auto_now_add=True)
 
     
+    def __str__(self):
+        return self.titulo[:30]
 
-# Create your models here.
